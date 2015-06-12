f6b660b93e08006762a64c92b8e0920bee5b8994
James Donaldson <jamdon@uw.edu>
2015-06-12 07:40:35 -0700

    update Gruntfile with working jst and browserify tasks; update package.json with application dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,26 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		jst: {
-			namespace: 'TFT'
-		},
-		files: {
-			'app/public/js/templates/templates.js': ['**/**/*.tpl']
-		}
+            compile: {
+                options: {
+                    namespace: 'TFT',
+                    processName: function (filepath) {
+                        return filepath.replace(/(app\/public\/js\/templates\/|\.tpl)/ig, '');
+                    }
+                },
+                files: {
+                    'app/public/js/templates/templates.js': 'app/public/js/templates/*.tpl'
+                }
+            }
+        },
+        browserify: {
+        	app: {
+        		src: 'app/public/js/main.js',
+        		dest: 'app/public/js/main.bundled.js'
+        	}
+        }
 	});
 	grunt.loadNpmTasks('grunt-contrib-jst');
-	grunt.registerTask('default', ['jst']);
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.registerTask('default', ['jst', 'browserify']);
 };
