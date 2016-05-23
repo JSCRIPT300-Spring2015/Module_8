e05fd1df4a313b0014e04c6a9ba1ad70f67c1929
jamdon <jamdon@uw.edu>
2016-05-23 16:17:15 -0700

    fix indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,24 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		jst: {
-            compile: {
-                options: {
-                    namespace: 'TFT',
-                    processName: function (filepath) {
-                        return filepath.replace(/(app\/public\/js\/templates\/|\.tpl)/ig, '');
-                    }
-                },
-                files: {
-                    'app/public/js/templates/templates.js': 'app/public/js/templates/*.tpl'
-                }
-            }
-        },
-        browserify: {
-        	app: {
-        		src: 'app/public/js/main.js',
-        		dest: 'app/public/js/main.bundled.js'
-        	}
-        }
+	            compile: {
+	                options: {
+	                    namespace: 'TFT',
+	                    processName: function (filepath) {
+	                        return filepath.replace(/(app\/public\/js\/templates\/|\.tpl)/ig, '');
+	                    }
+	                },
+	                files: {
+	                    'app/public/js/templates/templates.js': 'app/public/js/templates/*.tpl'
+	                }
+	            }
+	        },
+	        browserify: {
+	        	app: {
+	        		src: 'app/public/js/main.js',
+	        		dest: 'app/public/js/main.bundled.js'
+	        	}
+	        }
 	});
 	grunt.loadNpmTasks('grunt-contrib-jst');
 	grunt.loadNpmTasks('grunt-browserify');
